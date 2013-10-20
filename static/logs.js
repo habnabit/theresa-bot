73a89eb2e89e32b233cd431e425c8a69d20d8c60
Aaron Gallagher <_@habnab.it>
2013-10-20 00:35:44 -0700

    Add smooth scrolling.

--- static/logs.js
+++ static/logs.js
@@ -13,6 +13,35 @@ function parseMessage(s) {
 
 var lastDirection = undefined;
 var lastBox = undefined;
+var scrollData = {
+    startedAt: undefined,
+    start: undefined,
+    width: undefined,
+    interval: undefined,
+};
+
+function now() {
+    return (new Date()).getTime();
+}
+
+function scrollALittle() {
+    var delta = (now() - scrollData.startedAt) / 200;
+    if (delta > 1) {
+        clearInterval(scrollData.interval);
+        var coeff = 1;
+    } else {
+        var coeff = (-Math.cos(Math.PI * delta) + 1) / 2;
+    }
+    window.scrollTo(0, scrollData.start + scrollData.width * coeff);
+}
+
+function scrollToPosition(pos) {
+    scrollData.startedAt = now();
+    scrollData.start = pageYOffset;
+    scrollData.width = pos - pageYOffset;
+    clearInterval(scrollData.interval);
+    scrollData.interval = setInterval(scrollALittle, 10);
+}
 
 source.onmessage = function (event) {
     var doScroll = document.body.scrollHeight - innerHeight - pageYOffset <= 64;
@@ -28,5 +57,5 @@ source.onmessage = function (event) {
     newNode.className = data.isText? 'message' : 'system';
     lastBox.appendChild(newNode);
     if (doScroll)
-        window.scrollTo(0, document.body.scrollHeight);
+        scrollToPosition(document.body.scrollHeight - innerHeight);
 }
